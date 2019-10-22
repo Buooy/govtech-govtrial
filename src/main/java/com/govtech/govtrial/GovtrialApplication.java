50bafe7c4b30426b175c95c430563132208079f2
Aaron Stevenson Lee <aaron.lee@buooy.com>
2019-10-22 13:28:27 +0800

    Added : First Commit

---
+++ src/main/java/com/govtech/govtrial/GovtrialApplication.java
@@ -0,0 +1,13 @@
+package com.govtech.govtrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovtrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovtrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/govtech/govtrial/GovtrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govtech.govtrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovtrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
